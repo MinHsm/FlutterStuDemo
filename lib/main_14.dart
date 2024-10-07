a089eaf96c8394e314c78450002b6319a362a486
ming <2359848791@qq.com>
2024-10-07 19:07:55 +0800

    wrap

--- lib/main_14.dart
+++ lib/main_14.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: "Flutter Demo",
       home: Scaffold(
         appBar: AppBar(title: const Text("Flutter app")),
-        body: const MyHomePage(),
+        body: const LayoutDemo(),
       ),
     );
   }
@@ -25,155 +25,105 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return mButton(
+      "第一集",
+      onPressed: () {},
+    );
+  }
+}
+
+class mButton extends StatelessWidget {
+  String text; //按钮文字
+  void Function()? onPressed; //方法
+
+  mButton(this.text, {super.key, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ButtonStyle(
+          shape: WidgetStateProperty.all(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(1))),
+          backgroundColor:
+              WidgetStateProperty.all(const Color.fromARGB(242, 255, 244, 244)),
+          foregroundColor: WidgetStateProperty.all(Colors.black45)),
+      onPressed: onPressed,
+      child: Text(text),
+    );
+  }
+}
+
+class LayoutDemo extends StatelessWidget {
+  const LayoutDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return ListView(
+      padding: const EdgeInsets.all(10),
       children: [
         Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            ElevatedButton(
-              onPressed: () {
-                print("ElevatedButton");
-              },
-              child: const Text("普通按钮"),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: const Text("文本按钮"),
-            ),
-            const OutlinedButton(
-              onPressed: null,
-              child: Text("边框按钮"),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.thumb_up),
+            Text(
+              "热搜",
+              style: Theme.of(context).textTheme.titleLarge,
             )
+          ],
+        ),
+        const Divider(),
+        Wrap(
+          spacing: 10,
+          runSpacing: 10,
+          children: [
+            mButton("女装", onPressed: () {}),
+            mButton("女装", onPressed: () {}),
+            mButton("女装", onPressed: () {}),
+            mButton("女装", onPressed: () {}),
+            mButton("女装", onPressed: () {}),
+            mButton("女装", onPressed: () {}),
+            mButton("手机", onPressed: () {}),
+            mButton("xxxx", onPressed: () {}),
           ],
         ),
         const SizedBox(
           height: 20,
         ),
         Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            ElevatedButton.icon(
-              onPressed: () {},
-              icon: const Icon(Icons.send),
-              label: const Text("发送"),
+            Text(
+              "历史记录",
+              style: Theme.of(context).textTheme.titleLarge,
+            )
+          ],
+        ),
+        const Divider(),
+        const Column(
+          children: [
+            ListTile(
+              title: Text("女装"),
             ),
-            TextButton.icon(
-              onPressed: () {},
-              label: const Text("消息"),
-              icon: const Icon(Icons.info),
+            Divider(),
+            ListTile(
+              title: Text("手机"),
             ),
-            OutlinedButton.icon(
-              onPressed: null,
-              label: const Text("增加"),
-              icon: const Icon(Icons.add),
+            Divider(),
+            ListTile(
+              title: Text("电脑"),
             )
           ],
         ),
         const SizedBox(
           height: 20,
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            ElevatedButton(
+        Padding(
+          padding: const EdgeInsets.all(40),
+          child: OutlinedButton.icon(
               style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.all(Colors.red),
-                  foregroundColor: WidgetStateProperty.all(Colors.black)),
-              onPressed: () {
-                print("ElevatedButton");
-              },
-              child: const Text("普通按钮"),
-            )
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              height: 60,
-              width: 200,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text("大按钮"),
-              ),
-            ),
-            SizedBox(
-              height: 60,
-              width: 200,
-              child: ElevatedButton.icon(
-                onPressed: () {},
-                icon: const Icon(Icons.search),
-                label: const Text("搜索"),
-              ),
-            )
-          ],
-        ),
-        Row(
-          children: [
-            Expanded(
-              flex: 1,
-              child: Container(
-                margin: const EdgeInsets.all(10),
-                height: 60,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                    backgroundColor: WidgetStateProperty.all(Colors.redAccent),
-                    foregroundColor: WidgetStateProperty.all(Colors.white),
-                  ),
-                  child: const Text("登录"),
-                ),
-              ),
-            )
-          ],
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            ElevatedButton(
-              style: ButtonStyle(
-                shape: WidgetStateProperty.all(RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(0),
-                )),
-              ),
+                  foregroundColor: WidgetStateProperty.all(Colors.black45),
+                  shape: WidgetStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(1)))),
               onPressed: () {},
-              child: const Text("圆角按钮"),
-            ),
-            SizedBox(
-              height: 80,
-              width: 80,
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  shape: WidgetStateProperty.all(const CircleBorder(
-                      side: BorderSide(color: Colors.yellow))),
-                ),
-                onPressed: () {},
-                child: const Text("圆角"),
-              ),
-            )
-          ],
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            OutlinedButton(
-              style: ButtonStyle(
-                  side: WidgetStateProperty.all(
-                      const BorderSide(width: 2, color: Colors.redAccent))),
-              onPressed: () {},
-              child: const Text("带边框的按钮"),
-            )
-          ],
+              icon: const Icon(Icons.delete),
+              label: const Text("清空历史记录")),
         )
       ],
     );
---
+++ lib/main_15.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+//代码块 stateless
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Demo",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(),
+    );
+  }
+}
+
+//有状态组件
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
